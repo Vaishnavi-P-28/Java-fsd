954ab0aac5246d3decefc086a02055ca0bfaf95b
Vaishnavi-P-28 <109958303+Vaishnavi-P-28@users.noreply.github.com>
2022-07-26 21:07:52 +0530

    first commit

---
+++ implicit and explicit/src/explicit.java
@@ -0,0 +1,12 @@
+
+public class explicit {
+	
+	    public static void main(String[] args) {
+	        
+	        int a=257;
+	        byte b =(byte)a;
+	        System.out.print(b);
+	    }
+	    
+	}
+
